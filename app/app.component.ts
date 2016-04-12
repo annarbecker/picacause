3dc5f8e0757badd06a4e198d6cac5bd999485970
Anna Becker and Dustin Davis <anna.r.becker@gmail.com>
2016-04-12 13:34:59 -0700

    Add instagram api call functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from 'angular2/core';
+import {Observable} from 'rxjs/Rx';
+import {Http, Response} from 'angular2/http';
 import { CharityListComponent } from './charity-list.component';
 
 @Component({
@@ -18,9 +20,31 @@ import { CharityListComponent } from './charity-list.component';
     <p>All Cards</p>
     <button class="cardsHomeSlide">Home</button>
   </div>
+  <br>
+  <h2>API test</h2>
+  <ul *ngFor="#currentPic of pics">
+    <img src="{{currentPic.images.standard_resolution.url}}">
+  </ul>
     `
 })
 
 export class AppComponent {
+  public pics = [];
 
+  constructor(private http:Http) {}
+
+  ngOnInit() {
+    this.getFoods();
+  }
+
+  getFoods() {
+    return this.http.get('https://api.instagram.com/v1/tags/charityapp/media/recent?access_token=3128477430.8c5216d.5551b14da14a40ed9c77579a4d83484e').map((res:Response) => res.json()).subscribe(
+      // the first argument is a function which runs on success
+      data => { this.pics = data.data},
+      // the second argument is a function which runs on error
+      err => console.error(err),
+      // the third argument is a function which runs on completion
+      () => console.log(this.pics)
+    );
+  }
 }
