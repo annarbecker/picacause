33b6cff914ebbfa8c090f7523c6bb8a6b8895720
Anna Becker and Dustin Davis <hiitsdustindavis@gmail.com>
2016-04-12 09:13:22 -0700

    Build charity model

---
+++ app/charity.model.ts
@@ -0,0 +1,3 @@
+export class Charity {
+  constructor(public name: string, public mission: string, public image: string, public contact: string, public goal: number, public secured: number, public category: string, public hashtag: string) {}
+}
