c35c960f1f6354c980a3790ba547567458526bfa
Matt Rosanio and Tyler Suderman <mmrosani@syr.edu>
2016-04-14 11:01:21 -0700

    Add polish to login component

--- app/admin-login.component.ts
+++ app/admin-login.component.ts
@@ -13,9 +13,20 @@ import {NewCharityComponent} from './new-charity.component';
       <input placeholder="password" type="password" #adminPassword>
       <button (click)="loginAdmin(adminUsername, adminPassword)">Go!</button>
     </div>
+    <div class="userNotFound">
+      <p>A user by that name could not be found</p>
+    </div>
+    <div class="loginFailed">
+      <p>Password was incorrect</p>
+    </div>
     <div class="adminPage">
       <h4>It works</h4>
       <new-charity (onSubmitNewCharity)="createCharity($event)"></new-charity>
+      <h4>Sign up new admin</h4>
+      <input placeholder="username" #newAdminUsername>
+      <input placeholder="password" type="password" #newAdminPassword>
+      <button (click)="createAdmin(newAdminUsername, newAdminPassword)">Go!</button>
+      <button (click)="logout()">Logout</button>
     </div>
   `
 })
@@ -29,39 +40,45 @@ export class AdminLoginComponent {
     this.onSubmitNewCharity = new EventEmitter();
   }
 
-  ngOnInit() {
-    var adminClass = this;
-
-    //this event emitter fires once on page load and every time firebase is updated
-    adminClass.myDataRef.on("value", function(snapshot) {
-      //save results to array
-      var admins = snapshot.val().admins;
-      for(var key in admins) {
-        //Skip loop if property is from prototype
-        if(!admins.hasOwnProperty(key)) continue;
-        //save and push each object in admins to charityList
-        var obj = admins[key];
-        adminClass.adminList.push(obj);
-      }
-    }, function(errorObject) {
-      console.log("The read failed: " + errorObject.code);
-    });
-
-  }
-
   loginAdmin(username: HTMLInputElement, password: HTMLInputElement) {
     var adminsRef = this.myDataRef.child("admins");
     var adminClass = this;
+    var userFound = false;
     adminsRef.orderByChild('username').equalTo(username.value).on('child_added', function(snapshot) {
+      userFound = true;
       if(snapshot.val().password === password.value) {
         $('.login').fadeOut(100, function() {
           $('.adminPage').fadeIn(100);
         });
+        $('.loginFailed').hide();
+        $('.userNotFound').hide();
+      } else {
+        $('.loginFailed').show();
+        $('.userNotFound').hide();
       }
     });
+    if(!userFound) {
+      $('.userNotFound').show();
+    }
   }
 
   createCharity(charityArray: Array<any>) {
     this.onSubmitNewCharity.emit(charityArray);
+  }
+
+  createAdmin(username: HTMLInputElement, password: HTMLInputElement) {
+    var adminsRef = this.myDataRef.child("admins");
+    adminsRef.push({
+      username: username.value,
+      password: password.value
+    });
+    username.value = "";
+    password.value = "";
+  }
+
+  logout() {
+    $('.adminPage').fadeOut(100, function() {
+      $('.login').fadeIn(100);
+    });
   }
 }
