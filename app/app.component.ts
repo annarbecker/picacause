b8397b7ef3774e5ec0c7e0044a520b7b5cc98041
Dustin <hiitsdustindavis@gmail.com>
2016-04-14 16:46:51 -0700

    add nav collapse

--- app/app.component.ts
+++ app/app.component.ts
@@ -19,17 +19,19 @@ import {AdminLoginComponent} from './admin-login.component';
   <header>
     <div class="container header">
       <p class="logo homeSlide"><span>pic</span><span class="logo-medblue">a</span><span class="logo-lightblue"span>cause</span></p>
-      <ul class="navvy">
-        <li><a class="homeSlide" href="#">home</a></li>
-        <li><a data-toggle="modal" data-target="#myModal">about</a></li>
-        <li><a href="#">contact</a></li>
-        <li><a class="adminFade" href="#">apply</a></li>
-      </ul>
-      <a href="#" class="homeCartShow nav-cart-icon">
-      <svg-cart class=svgCart></svg-cart>
-      <p id="cart-count">{{cartCount}}</p>
-      <p id="open-cart-hover">open cart</p>
-      </a>
+      <div class="navvy">
+        <ul>
+          <li><a class="homeSlide" href="#">home</a></li>
+          <li><a data-toggle="modal" data-target="#myModal">about</a></li>
+          <li><a href="#">contact</a></li>
+          <li><a class="adminFade" href="#">apply</a></li>
+        </ul>
+        <a href="#" class="homeCartShow nav-cart-icon">
+          <svg-cart class=svgCart></svg-cart>
+          <p id="cart-count">{{cartCount}}</p>
+          <p id="open-cart-hover">open cart</p>
+        </a>
+      </div>
     </div>
   </header>
 
