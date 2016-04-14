59ff84f620f70124512951a3cb66bb7e1a127fff
Anna Becker and Dustin Davis <anna.r.becker@gmail.com>
2016-04-14 13:21:21 -0700

    Fix merge conflicts

--- app/charity-list.component.ts
+++ app/charity-list.component.ts
@@ -46,26 +46,7 @@ export class CharityListComponent {
       console.log("The read failed: " + errorObject.code);
     });
   }
-<<<<<<< HEAD
-  createCharity(charityArray: Array<any>): void {
-    // this.charityList.push (
-    //   new Charity(charityArray[0], charityArray[1], charityArray[2], charityArray[3], charityArray[4], 0, charityArray[5], charityArray[6])
-    // );
-    var charitiesRef = this.myDataRef.child("charities");
-    charitiesRef.push({
-      name: charityArray[0],
-      mission: charityArray[1],
-      image: charityArray[2],
-      contact: charityArray[3],
-      goal: charityArray[4],
-      secured: 0,
-      category: charityArray[5],
-      hashtag: charityArray[6]
-    });
-  }
-=======
 
->>>>>>> 876f7d27e0e1b757c75cded0f29fe7b54d7292f9
   charityClicked(clickedCharity: Charity) {
     if(this.selectedCharity === clickedCharity) {
       this.selectedCharity = undefined;
